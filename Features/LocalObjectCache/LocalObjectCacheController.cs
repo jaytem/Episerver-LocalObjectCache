7674d6469a765b6bc78e44a3ffff82eba3b4b09e
Joe Mayberry <joe.mayberry@episerver.com>
2019-02-19 09:00:44 -0700

    Updated LocalObjectCacheController to use ISynchronizedObjectInstanceCache abstraction instead of the CacheManager, and injecting it using Dependency Injection

--- Features/LocalObjectCache/LocalObjectCacheController.cs
+++ Features/LocalObjectCache/LocalObjectCacheController.cs
@@ -4,45 +4,52 @@ using LOC.Cache.Models.ViewModels;
 using EPiServer.PlugIn;
 using System.Collections;
 using EPiServer.Core;
-using EPiServer;
+using EPiServer.Framework.Cache;
 
 namespace LOC.Cache.Controllers
 {
 	[Authorize(Roles = "CmsAdmins")]
 	[GuiPlugIn(Area = PlugInArea.AdminMenu, Url = "~/localobjectcache", DisplayName = "Clear Local Object Cache")]
-    public class LocalObjectCacheController : Controller
-    {
-        // GET: ObjectCache
-    public ActionResult Index(string FilteredBy)
-    {
-		var viewmodel = new LocalObjectCacheViewModel();
+	public class LocalObjectCacheController : Controller
+	{
+		private readonly ISynchronizedObjectInstanceCache cache;
 
-		var cachedEntries = HttpContext.Cache.Cast<DictionaryEntry>();
-			
-		switch (FilteredBy)
+		public LocalObjectCacheController(ISynchronizedObjectInstanceCache cache)
 		{
-			case "pages":
-				viewmodel.CachedItems = cachedEntries.Where(item => item.Value is PageData);
-				break;
-			case "content":
-				viewmodel.CachedItems = cachedEntries.Where(item => item.Value is IContent);
-				break;
-			default:
-				viewmodel.CachedItems = cachedEntries;
-				break;
+			this.cache = cache;
 		}
 
-		viewmodel.FilteredBy = FilteredBy;
-
-		viewmodel.Choices = new[]
+		public ActionResult Index(string FilteredBy)
 		{
-			new SelectListItem { Text = "All Cached Objects", Value = "all" },
-			new SelectListItem { Text = "Any Content", Value = "content" },
-			new SelectListItem { Text = "Pages Only", Value = "pages" }
-		};
+			var viewmodel = new LocalObjectCacheViewModel();
 
-        return View("~/Features/LocalObjectCache/LocalObjectCache.cshtml", viewmodel);
-    }
+			var cachedEntries = HttpContext.Cache.Cast<DictionaryEntry>();
+
+			switch (FilteredBy)
+			{
+				case "pages":
+					viewmodel.CachedItems = cachedEntries.Where(item => item.Value is PageData);
+					break;
+				case "content":
+					viewmodel.CachedItems = cachedEntries.Where(item => item.Value is IContent);
+					break;
+				default:
+					viewmodel.CachedItems = cachedEntries;
+					break;
+			}
+
+			viewmodel.FilteredBy = FilteredBy;
+
+			viewmodel.Choices = new[]
+			{
+				new SelectListItem { Text = "All Cached Objects", Value = "all" },
+				new SelectListItem { Text = "Any Content", Value = "content" },
+				new SelectListItem { Text = "Pages Only", Value = "pages" }
+			};
+
+			return View("~/Features/LocalObjectCache/LocalObjectCache.cshtml", viewmodel);
+		}
+
 
 		public ActionResult RemoveLocalCache(string[] cacheKey)
 		{
@@ -50,10 +57,10 @@ namespace LOC.Cache.Controllers
 			{
 				foreach (string key in cacheKey)
 				{
-					CacheManager.RemoveLocalOnly(key);
+					cache.RemoveLocal(key);
 				}
 			}
 			return RedirectToAction("Index");
 		}
-    }
+	}
 }
